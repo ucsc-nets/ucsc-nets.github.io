7b6dc93a0bd9908c3a726119426a4ce24c2a7adf
popov-andrew <ucsc.nets@gmail.com>
2026-05-02 02:55:04 -0700

    setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ucsc-nets.github.io
+Aye we got React + Nextjs with a little tailwindcss
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ src/app/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <div>
+        </div>
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-sm text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            Treeneets at <br/>
+            University of California, Santa Cruz
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            This page is still a work in progress
+
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex w-full p-2 items-center justify-center gap-2 rounded-xl bg-foreground text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
+            href="https://www.instagram.com/ucsc.nets"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="light:invert"
+              src="/instagram.svg"
+              alt="Instagram logo"
+              width={32}
+              height={32}
+            />
+          </a>
+          <a
+            className="flex w-full p-2 items-center justify-center gap-2 rounded-xl bg-foreground text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc]"
+            href="https://www.instagram.com/ucsc_nature"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="light:invert"
+              src="/instagram.svg"
+              alt="Instagram logo"
+              width={32}
+              height={32}
+            />
+          </a>
+        </div>
+        <div>
+          <p className="-mt-24">
+            Stay up to date on Instagram
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
